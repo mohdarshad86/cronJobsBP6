fab3eb7de4ab284a383d25fd41502b236c3e79ca
Mohd Arshad <mohdarshad8668@gmail.com>
2023-02-11 17:13:19 +0530

    added function and promise

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const schedule = require('node-schedule')
+const cron = require("node-cron");
 
 
 //At particular date and time
-const someDate = new Date('2023-02-11T13:34:00.000+5:30')
+// const someDate = new Date('2023-02-11T1:21:00.000+5:30')
 
 // schedule.scheduleJob(someDate, ()=>{
 //     console.log('Job ran @', new Date().toString())
@@ -13,11 +13,45 @@ const someDate = new Date('2023-02-11T13:34:00.000+5:30')
 // schedule.scheduleJob('m-job','*/7 * * * * *', ()=>{
 //     console.log('I will run after every 7sec ...');
 
-//     // to cancel the job schedule.cancelJob('m-job')
+//     // to cancel the job 
+//     schedule.cancelJob('m-job')
 // })
 
-const mjob = schedule.scheduleJob('*/7 * * * * *', ()=>{
-    console.log('I will run after every 7sec ...');
+// const mjob = schedule.scheduleJob('*/7 * * * * *', ()=>{
+//     console.log('I will run after every 7sec ...');
 
-    mjob.cancel()
-})
+//     mjob.cancel()
+// })
+
+
+const events = [
+    { text: "event 1", dateTime: "* * * * *" },
+    { text: "event 2", dateTime: "* * * * *" },
+    { text: "event 3", dateTime: "* * * * *" },
+    { text: "event 4", dateTime: "* * * * *" },
+    { text: "event 5", dateTime: "* * * * *" },
+    { text: "event 6", dateTime: "* * * * *" },
+    { text: "event 7", dateTime: "* * * * *" },
+    { text: "event 8", dateTime: "* * * * *" },
+    { text: "event 9", dateTime: "* * * * *" },
+    { text: "event 10", dateTime: "* * * * *" }
+];
+
+async function runEvents(events) {
+    for (const event of events) {
+        await triggerFunction(event.text);
+    }
+}
+
+async function triggerFunction(text) {
+    await sleep(text.length * 1000);
+    console.log(text.split("").reverse().join(""));
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+cron.schedule("* * * * *", () => {
+    runEvents(events);
+});
